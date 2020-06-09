d8c1af47b7e11844e13d1d0a097a9391b50d60db
AdnoC <adam.r.cutler@gmail.com>
2020-06-08 22:25:34 -0400

    ACTUALLY ADD THE RESULTS PACKAGE GIT

---
+++ src/main/java/com/deque/html/axecore/results/Node.java
@@ -0,0 +1,22 @@
+package com.deque.html.axecore.results;
+
+public class Node {
+    private String html;
+    private Object target;
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(final String html) {
+        this.html = html;
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+
+    public void setTarget(final Object target) {
+        this.target = target;
+    }
+}
