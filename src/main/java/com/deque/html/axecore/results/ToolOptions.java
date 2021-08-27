ee3b0e7d31324544e856754af0c2493fb85a9b33
AdnoC <adam.r.cutler@gmail.com>
2021-08-27 05:51:57 -0400

    add missing classes

---
+++ src/main/java/com/deque/html/axecore/results/ToolOptions.java
@@ -0,0 +1,14 @@
+package com.deque.html.axecore.results;
+
+public class ToolOptions {
+  private String reporter;
+
+  public String getReporter() {
+    return reporter;
+  }
+
+  public void setReporter(String reporter) {
+    this.reporter = reporter;
+  }
+}
+
