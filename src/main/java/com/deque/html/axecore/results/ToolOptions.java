3ce114446485f3ad95fc942c756c911b26e7386e
Adam Cutler <adam.r.cutler@gmail.com>
2021-09-16 15:57:31 -0400

    chore: udpate ToolOptions for json (#182)

--- src/main/java/com/deque/html/axecore/results/ToolOptions.java
+++ src/main/java/com/deque/html/axecore/results/ToolOptions.java
@@ -1,8 +1,16 @@
 package com.deque.html.axecore.results;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class ToolOptions {
   private String reporter;
   private Object rules;
+  private Map<String, Object> properties;
 
   public String getReporter() {
     return reporter;
@@ -17,6 +25,19 @@ public class ToolOptions {
   }
   public void setRules(Object rules) {
     this.rules = rules;
+  }
+
+  @JsonAnyGetter
+  public Map<String, Object> getProperties() {
+    return properties;
+  }
+ 
+  @JsonAnySetter
+  public void setProperty(String name, Object value) {
+    if (properties == null) {
+      properties = new HashMap<String, Object>();
+    }
+    properties.put(name, value);
   }
 }
 
