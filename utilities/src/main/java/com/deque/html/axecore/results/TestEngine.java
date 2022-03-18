31e7742bc9ea4d1f87c1c560861935a1d3a5b8ec
Gabe <41127686+Zidious@users.noreply.github.com>
2022-03-18 20:48:57 +0000

    new submodule: utilities which contains all common JSON mapped classes used in all packages

---
+++ utilities/src/main/java/com/deque/html/axecore/results/TestEngine.java
@@ -0,0 +1,18 @@
+package com.deque.html.axecore.results;
+
+public class TestEngine {
+  private String name;
+  private String version;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+}
---
+++ utilities/src/main/java/com/deque/html/axecore/results/TestEnvironment.java
@@ -0,0 +1,29 @@
+package com.deque.html.axecore.results;
+
+public class TestEnvironment {
+  private String userAgent;
+  private int windowWidth;
+  private int windowHeight;
+  private double orientationAngle;
+  private String orientationType;
+
+  public String getUserAgent() {
+    return userAgent;
+  }
+
+  public int getWindowHeight() {
+    return windowHeight;
+  }
+
+  public int getwindowWidth() {
+    return windowWidth;
+  }
+
+  public double getOrientationAngle() {
+    return orientationAngle;
+  }
+
+  public String getOrientationType() {
+    return orientationType;
+  }
+}
