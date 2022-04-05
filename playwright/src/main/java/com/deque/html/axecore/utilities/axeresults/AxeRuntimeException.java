8344113718726e333f340a35e8f5ae70632cadb5
Gabe <41127686+Zidious@users.noreply.github.com>
2022-04-05 14:49:38 +0100

    refactor(playwright): use dequeUtitilies sub module for JSON mapping classes

--- playwright/src/main/java/com/deque/html/axecore/utilities/axeresults/AxeRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package com.deque.html.axecore.utilities.axeresults;
-
-public class AxeRuntimeException extends RuntimeException {
-  private static final long serialVersionUID = -123456789087654L;
-
-  public AxeRuntimeException(Exception cause) {
-    super("Error when running axe", cause);
-  }
-}
