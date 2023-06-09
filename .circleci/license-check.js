e4e9b3cccea714b0b8e19fc1bb44e45011f10a0b
Gabe <41127686+Zidious@users.noreply.github.com>
2023-06-09 18:49:24 +0100

    check if license checking actually fails

--- .circleci/license-check.js
+++ .circleci/license-check.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const filePath = process.argv[2];
-const file = fs.readFileSync(filePath, { 'encoding': 'utf-8' });
+const file = fs.readFileSync(filePath, { encoding: "utf-8" });
 
 const gplRegex = /gpl/i;
-for (const line of file.split('\n')) {
-  const parts = line.split('\t');
+for (const line of file.split("\n")) {
+  const parts = line.split("\t");
   if (parts.length < 2) {
     continue;
   }
@@ -16,9 +16,9 @@ for (const line of file.split('\n')) {
       hasNonGPL = true;
     }
   }
-  if (!hasNonGPL) {
-    console.log("Found dependency with GPL as sole license.");
-    console.log(parts[0]);
-    process.exit(1);
-  }
+  // if (!hasNonGPL) {
+  console.log("Found dependency with GPL as sole license.");
+  console.log(parts[0]);
+  process.exit(1);
+  // }
 }
