9c5c94e2c10d57bfadd7cf3666c55850d9618220
Zidious <gaby1306@gmail.com>
2023-11-18 05:36:46 +0000

    whoops, forgot to import chromeoptions class

--- selenium/src/test/java/com/deque/html/axecore/extensions/WebDriverExtensionsTest.java
+++ selenium/src/test/java/com/deque/html/axecore/extensions/WebDriverExtensionsTest.java
@@ -6,6 +6,7 @@ import org.junit.Assume;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
